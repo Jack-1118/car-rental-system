fe08f40da19d972816312057ef7ef2e4d7b19cd9
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-05-01 22:45:47 +0800

    user register backup file still on working

--- app/src/main/java/carrentalsystem/service/UserService.java
+++ app/src/main/java/carrentalsystem/service/UserService.java
@@ -4,9 +4,63 @@ import java.util.List;
 
 import carrentalsystem.dao.UserDAO;
 import carrentalsystem.model.User;
-
+import java.io.IOException;
 
 public class UserService {
+
+    private UserService() {
+        // Private constructor to prevent instantiation
+    }
+
+    //Register
+    public static String register(String username, String password){
+        boolean usernameChecker = false ;
+        boolean passwordChecker = false ;
+
+        
+        if (username == null || username.trim().isEmpty()) {
+            return "Username cannot be empty";
+        }
+        
+        List<User> users = UserDAO.loadUsers();
+
+        for (User user: users){
+            if(user.getUsername().equals(username)){
+                return "Username already been taken";
+            }
+        }
+        usernameChecker = true;
+        
+        if(password == null || password.trim().isEmpty()){
+            return "Password cannot be empty";
+        } else if (password.length() < 8){
+            return "Password must be at least 8 character";
+        }
+        passwordChecker = true;
+        
+        // If both username and password are valid, register the user
+        if (usernameChecker && passwordChecker) {
+            
+                // Create a new User object with provided username and password
+                
+                User user = new User(username, password);
+                
+                // Save the user
+                UserDAO.saveUser(user);
+                
+                return "Success";
+            } 
+        return "Error occurred while registering";
+            
+        
+
+      
+    }
+        
+
+
+    
+    
 
     //Login
     public static String login(String username, String password) {
