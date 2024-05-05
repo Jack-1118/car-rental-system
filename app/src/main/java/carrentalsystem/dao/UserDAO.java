bac96c2183bf054547d2f07c1b31ffe2f8eea6c1
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-05-05 23:41:53 +0800

    added function for save admin and load admin

--- app/src/main/java/carrentalsystem/dao/UserDAO.java
+++ app/src/main/java/carrentalsystem/dao/UserDAO.java
@@ -9,6 +9,7 @@ import carrentalsystem.util.FileUtil;
 
 public class UserDAO {
     private static final String DATA_FILE_PATH = "app/src/main/resources/UserData.txt";
+    private static final String DATA_FILE_PATH_ADMIN = "app/src/main/resources/AdminData.txt";
 
     public static void saveUser(User user) {
         FileUtil.saveFile(DATA_FILE_PATH, Collections.singletonList(user));
@@ -16,6 +17,14 @@ public class UserDAO {
 
     public static List<User> loadUsers() {
         return FileUtil.loadFile(DATA_FILE_PATH, User.class);
+    }
+
+    public static void saveAdmin(User user) {
+        FileUtil.saveFile(DATA_FILE_PATH_ADMIN, Collections.singletonList(user));
+    }
+
+    public static List<User> loadAdmins() {
+        return FileUtil.loadFile(DATA_FILE_PATH_ADMIN, User.class);
     }
 
 }
