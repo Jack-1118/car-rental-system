b8113c0c3d8ea0e6cb51c79fc7d95505c0d1debc
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-05-14 09:15:22 +0800

    remove admin loign

--- app/src/main/java/carrentalsystem/service/AdminService.java
+++ app/src/main/java/carrentalsystem/service/AdminService.java
@@ -6,19 +6,19 @@ import carrentalsystem.dao.UserDAO;
 import carrentalsystem.model.User;
 
 public class AdminService {
-    //Login
-    public static String login(String username, String password) {
-        List<User> users = UserDAO.loadAdmins();
+    // //Login
+    // public static String login(String username, String password) {
+    //     List<User> users = UserDAO.loadAdmins();
 
-        // loop through the list of users to find the user with the matching username and password
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                return "admin";
-            } 
-        }
-        // return random value if no matching user is found
-        return "no";
+    //     // loop through the list of users to find the user with the matching username and password
+    //     for (User user : users) {
+    //         if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+    //             return "admin";
+    //         } 
+    //     }
+    //     // return random value if no matching user is found
+    //     return "no";
 
 
-    } 
+    // } 
 }
