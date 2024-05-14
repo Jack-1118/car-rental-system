cb2b9bf51638a124c2eb5388c49aa56102d76390
CHONG KELVIN <111684233+Kelocker@users.noreply.github.com>
2024-05-14 09:15:45 +0800

    remove user login

--- app/src/main/java/carrentalsystem/service/UserService.java
+++ app/src/main/java/carrentalsystem/service/UserService.java
@@ -12,29 +12,29 @@ import carrentalsystem.model.User;
 public class UserService {
     
 
-    //Login
-    public static String login(String username, String password) {
-        List<User> users = UserDAO.loadUsers();
+    // //Login
+    // public static String login(String username, String password) {
+    //     List<User> users = UserDAO.loadUsers();
 
-        // loop through the list of users to find the user with the matching username and password
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                UserDAO.saveSessionData(user);
-                return "customer";
-            } 
-        }
-        // return random value if no matching user is found
-        return "no";
+    //     // loop through the list of users to find the user with the matching username and password
+    //     for (User user : users) {
+    //         if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+    //             UserDAO.saveSessionData(user);
+    //             return "customer";
+    //         } 
+    //     }
+    //     // return random value if no matching user is found
+    //     return "no";
 
 
-    } 
+    // } 
 
 
 
     public static void main(String[] args) {
         // test the login method
-        System.out.println(login("admin1", "password1")); // admin
-        System.out.println(login("user1", "password1")); // user
-        System.out.println(login("user1", "password2")); // null
+        // System.out.println(login("admin1", "password1")); // admin
+        // System.out.println(login("user1", "password1")); // user
+        // System.out.println(login("user1", "password2")); // null
     }
 }
